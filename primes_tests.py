7f825eeb93c97bc29bb6604e75691383cc0378e8
Kenneth Lebu <kenlebu@gmail.com>
2017-04-18 13:26:57 +0300

    updated tests
    Updated the tests to adhere to the OOP standards of primes.py and to also do away with type errors

--- primes_tests.py
+++ primes_tests.py
@@ -2,21 +2,24 @@ import unittest
 from primes import PrimeGenerator
 
 class PrimeGeneratorTest(unittest.TestCase):
-    def setUp(self):
-        self.primes = PrimeGenerator()
+    
 
     def test_primes(self):
-        self.assertEqual(self.primes.prime_generator(10), [2, 3, 5, 7], msg="Not all numbers are prime")
-        self.assertEqual(self.primes.prime_generator(20), [2, 3, 5, 7, 11, 13, 17, 19], msg="Not all numbers are prime")
+        self.assertEqual(PrimeGenerator(10).prime_generator(), [2, 3, 5, 7])
+        self.assertEqual(PrimeGenerator(20).prime_generator(), [2, 3, 5, 7, 11, 13, 17, 19])
 
     def test_zero(self):
-        self.assertEqual(self.primes.prime_generator(0), "0 is not a prime number", msg="Zero is not a prime number")
+        self.assertEqual(PrimeGenerator(0).prime_generator(), "0 is not a prime number")
 
     def test_one(self):
-        self.assertEqual(self.primes.prime_generator(1), "1 is not a prime number", msg="One is not a prime number")
-        
+        self.assertEqual(PrimeGenerator(1).prime_generator(), "1 is not a prime number")
+
     def test_non_integer(self):
-        self.assertEqual(self.primes.prime_generator('a'), "Enter an integer", msg="Input must be an integer")
-        
+        self.assertEqual(PrimeGenerator('a').prime_generator(), "Enter an integer")
+
     def test_non_negative(self):
-        self.assertEqual(self.primes.prime_generator(-5), "Number should be a positive integer", msg="Input should be a positive integer")
+        self.assertEqual(PrimeGenerator(-5).prime_generator(), "Number should be a positive integer")
+
+
+if __name__ == '__main__':
+    unittest.main()
