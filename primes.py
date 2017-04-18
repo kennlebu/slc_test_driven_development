911ede572ce560a98578d01f4c453e3375ffa967
Kenneth Lebu <kenlebu@gmail.com>
2017-04-18 13:06:24 +0300

    removed unnecessary commented code

--- primes.py
+++ primes.py
@@ -12,17 +12,3 @@ class PrimeGenerator(object):
         if n == 1: return "1 is not a prime number"
 
         return [x for x in range(2,n+1) if isprime(x)]
-
-
-##import unittest
-##class prime_tester(unittest.TestCase):
-##    def test_if_primes(self):
-##        self.assertEqual(prime_generator(10), [2, 3, 5, 7])
-##        self.assertEqual(prime_generator(20), [2, 3, 5, 7, 11, 13, 17, 19])
-##
-##
-##
-##def main():
-##    prime_tester.test_if_primes()
-
-print(PrimeGenerator.prime_generator(0))
